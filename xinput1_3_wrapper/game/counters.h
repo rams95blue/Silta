383ff8a21bdebcf456c4492dbac3275df55d007a
rams <113950769+rams95blue@users.noreply.github.com>
2026-07-04 15:01:01 -0300

    Delete xinput1_3_wrapper directory

--- xinput1_3_wrapper/game/counters.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stdafx.h"
-#include "infra.h"
-namespace mod {
-	namespace counters {
-		void InitMapStats();
-		void StatSuccess(int event_type, int count, bool is_new);
-	}
-}
